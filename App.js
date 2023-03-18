c77a18848918fa9f5c4634b25cb23cee05d3544e
Your Name <eliisa.heino@myy.haaga-helia.fi>
2023-03-18 20:21:15 +0200

    valmis 18.3.2023

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] =useState('kattimatikainen');
+  const [selected, setSelected] = useState(0);
+
+  const langOptions =[
+    {label: "English", language: 'en-US'},
+    {label: "Finnish", language: 'fi-FI'},
+    {label: "Swedish", language: 'sv-SE'},
+  ];
+
+  const values = langOptions.map(option => option.label)
+
+  const speak = () => {
+    Speech.speak(text, {language: langOptions[selected].language});
+  };
+
+  console.log(langOptions[selected].language);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SegmentedControl
+        values={values}
+        selectedIndex={selected}
+        onChange={event => setSelected(event.nativeEvent.selectedSegmentIndex)}
+       />
+       <TextInput onChangeText={text => setText(text)} value={text}></TextInput>
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
